1fe5f856fd735985237ce42edd396c5dc13fa893
reedcreglow <61853840+reedcreglow@users.noreply.github.com>
2022-04-09 14:27:35 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// scroll to anchor link
+$(document).on('click', 'a[href^="#work"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
+
+// scroll arrow opacity from @robooneus on codepen.io
+$(window).scroll( function(){
+
+  //get scroll position
+  var topWindow = $(window).scrollTop();
+  //multipl by 1.5 so the arrow will become transparent half-way up the page
+  var topWindow = topWindow * 1.5;
+
+  //get height of window
+  var windowHeight = $(window).height();
+
+  //set position as percentage of how far the user has scrolled
+  var position = topWindow / windowHeight;
+  //invert the percentage
+  position = 1 - position;
+
+  //define arrow opacity as based on how far up the page the user has scrolled
+  //no scrolling = 1, half-way up the page = 0
+  $('.more-arrow').css('opacity', position);
+
+});
